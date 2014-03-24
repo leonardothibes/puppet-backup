78660025a90317bc7ba2d7f989d1a47ef8eff481
Leonardo Thibes <eu@leonardothibes.com>
2014-03-24 15:59:20 -0300

    implementando classe de backup simples que funciona minimamente bem #3

--- manifests/simple.pp
+++ manifests/simple.pp
@@ -9,42 +9,54 @@ define backup::simple(
 	$group      = 'root',
 	$mode       = 0644,
 	$compressor = 'tar.gz',
+	$dateformat = "%Y-%m-%d",
 	$from,
 	$to,
 ) {
 	include backup::params
 	include backup
-    case $ensure {
-        'present','absent': { $real_ensure = $ensure }
-        default           : { fail("Invalid value \"${ensure}\" used for ensure") }
-    }
-	$destination = "${backup::params::endpoint}/${to}"
+	case $ensure {
+		'present','absent': {}
+		default           : { fail("Invalid value \"${ensure}\" used for ensure") }
+	}
+	Exec {path => $backup::params::envpath}
 	if $ensure == 'present' {
 
-		# Step-1: copy
+		# Creating directory, if not exists
+		exec {"backup::directory::${to}::create":
+			command => "mkdir -p ${to}",
+			onlyif  => "[ ! -d ${to} ]",
+		}
+		# Creating directory, if not exists
+
+		# Making backup file name
+		$basename = basename($to)
+		$date     = strftime($dateformat)
+		$bkpname  = "${basename}_${date}"
+		$endpoint = "${to}/${bkpname}"
+		# Making backup file name
+
 		exec {"backup::simple::${title}::step-1":
-			path    => $backup::params::envpath,
-			command => "cp -Rf ${from} ${destination}",
-			onlyif  => "[ ! -d ${destination} ] && [ ! -f ${destination} ]",
-			before  => Exec["backup::simple::${title}::step-2"],
+			# Step-1: copy
+			command => "cp -Rf ${from} ${endpoint}",
+			onlyif  => "[ ! -d ${endpoint} ] && [ ! -f ${endpoint}.${compressor} ]",
+			require => Exec["backup::directory::${to}::create"],
+			# Step-1: copy
+		}->util::compress {"backup::simple::${title}::step-2":
+			# Step-2: compress
+			from => $from,
+			to   => "${endpoint}.${compressor}",
+			# Step-2: compress
+		}->exec {"backup::simple::${title}::step-3":
+			# Step-3: clean
+			command => "rm -Rf ${endpoint}",
+			onlyif  => "[ -f ${endpoint}.${compressor} ]"
+			# Step-3: clean
 		}
-		# Step-1: copy
-
-		# Step-2: compress
-		backup::compress {"backup::simple::${title}::step-2":
-			path       => $destination,
-			compressor => $compressor,
-			require    => Exec["backup::simple::${title}::step-1"],
-		}
-		# Step-2: compress
-
 	} else {
-		file {"backup::simple::file::${to}",:
-			ensure => absent,
-			path   => [
-				"${destination}.${compressor}",
-				$destination,
-			],
+		exec {"backup::simple::${to}::absent":
+			command => "rm -Rf ${to}",
+			onlyif  => "[ -f ${to} ]",
 		}
 	}
 }

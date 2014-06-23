5b5ce4886ed3f189167f5dce54fbcfe0873a95eb
Leonardo Thibes <eu@leonardothibes.com>
2014-06-23 11:59:08 -0300

    update

--- manifests/simple.pp
+++ manifests/simple.pp
@@ -10,6 +10,7 @@ define backup::simple(
 	$chmod      = 0644,
 	$compressor = 'tar.gz',
 	$dateformat = "%Y-%m-%d",
+	$log        = '/dev/null',
 	$from,
 	$to,
 ) {
@@ -21,54 +22,11 @@ define backup::simple(
 	}
 	Exec {path => $backup::params::envpath}
 	if $ensure == 'present' {
-
-		# Creating directory, if not exists
-		exec {"backup::directory::${to}::create":
-			command => "mkdir -p ${to}",
-			onlyif  => "[ ! -d ${to} ]",
-		}
-		# Creating directory, if not exists
-
-		# Making backup file name
-		$basename = basename($to)
-		$date     = strftime($dateformat)
-		$bkpname  = "${basename}_${date}"
-		$endpoint = "${to}/${bkpname}"
-		# Making backup file name
-
-		exec {"backup::simple::${title}::step-1":
-			# Step-1: copy
-			command => "cp -Rf ${from} ${endpoint}",
-			onlyif  => "[ ! -d ${endpoint} ] && [ ! -f ${endpoint}.${compressor} ]",
-			require => Exec["backup::directory::${to}::create"],
-			# Step-1: copy
-		}->util::compress {"backup::simple::${title}::step-2":
-			# Step-2: compress
-			from => $from,
-			to   => "${endpoint}.${compressor}",
-			# Step-2: compress
-		}->exec {"backup::simple::${title}::step-3":
-			# Step-3: clean
-			command => "rm -Rf ${endpoint}",
-			onlyif  => "[ -f ${endpoint}.${compressor} ]"
-			# Step-3: clean
-		}->util::chown {"backup::simple::${title}::step-4":
-			# Step-4: chown
-			file      => $to,
-			user      => $user,
-			group     => $group,
-			recursive => true,
-			# Step-4: chown
-		}->util::chmod {"backup::simple::${title}::step-5":
-			# Step-5: chmod
-			file => "${endpoint}.${compressor}",
-			mode => $chmod,
-			# Step-5: chmod
+		crontab::job {$name:
+			ensure => present,
+			
 		}
 	} else {
-		exec {"backup::simple::${to}::absent":
-			command => "rm -Rf ${to}",
-			onlyif  => "[ -f ${to} ]",
-		}
+		crontab::job {$name: ensure => absent}
 	}
 }
--- tests/init.pp
+++ tests/init.pp
@@ -9,7 +9,7 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-backup::simple {'backup of /tmp directory':
+backup::simple {'temp':
 	from  => '/tmp',
 	to    => '/backups/tmp',
 	user  => 'vagrant',

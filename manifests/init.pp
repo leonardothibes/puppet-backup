6b5eef8c168571774af6319766677c9789f87f50
Leonardo Thibes <eu@leonardothibes.com>
2014-06-23 16:26:45 -0300

    implementando classe principal do modulo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,14 @@
-class backup
+class backup inherits backup::params
 {
-	include backup::params
 	include backup::dependencies
+	
+	# Creatong backup directory
+	file {'backup::bkpdir':
+		ensure => directory,
+		owner  => root,
+		group  => root,
+		mode   => 0755,
+		path   => $backup::params::bkpdir,
+	}
+	# Creatong backup directory
 }
